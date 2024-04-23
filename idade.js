cb0200b167e9b9202cfa7f4b87fbe203c9c1d2ab
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-04-23 08:50:13 -0300

    adicionando os exercícios
    add files

---
+++ idade.js
@@ -0,0 +1,11 @@
+const idade = 15;
+
+
+if(idade >= 18){
+
+    console.log("Sua idade é: " + idade + ", você é maior de idade.");
+
+}else{
+
+    console.log("Sua idade é: " + idade + ", você é menor de idade.");
+}
---
+++ idade2.js
@@ -0,0 +1,3 @@
+const idade = 15;
+
+console.log("Sua idade é: " + idade + ", você é " + idade >=18? "maior de idade": "menor de idade")
