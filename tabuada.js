bce0cdc60324a092b03b5ac781e863f538172c7f
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-05-07 09:31:17 -0300

    Create tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,207 @@
+//PRIMEIRA TABUADA//
+console.log("==================================");
+
+console.log("1x1 = ", 1 * 1);
+console.log("1x2 = ", 1 * 2);
+console.log("1x3 = ", 1 * 3);
+console.log("1x4 = ", 1 * 4);
+console.log("1x5 = ", 1 * 5);
+console.log("1x6 = ", 1 * 6);
+console.log("1x7 = ", 1 * 7);
+console.log("1x8 = ", 1 * 8);
+console.log("1x9 = ", 1 * 9);
+console.log("1x10 = ",1 * 10);
+
+console.log("==================================");
+
+console.log("3x1 = ", 3 * 1);
+console.log("3x2 = ", 3 * 2);
+console.log("3x3 = ", 3 * 3);
+console.log("3x4 = ", 3 * 4);
+console.log("3x5 = ", 3 * 5);
+console.log("3x6 = ", 3 * 6);
+console.log("3x7 = ", 3 * 7);
+console.log("3x8 = ", 3 * 8);
+console.log("3x9 = ", 3 * 9);
+console.log("3x10 = ", 3 * 10);
+
+console.log("==================================");
+
+console.log("7x1 = ", 7 * 1);
+console.log("7x2 = ", 7 * 2);
+console.log("7x3 = ", 7 * 3);
+console.log("7x4 = ", 7 * 4);
+console.log("7x5 = ", 7 * 5);
+console.log("7x6 = ", 7 * 6);
+console.log("7x7 = ", 7 * 7);
+console.log("7x8 = ", 7 * 8);
+console.log("7x9 = ", 7 * 9);
+console.log("7x10 = ", 7 * 10);
+
+console.log("==================================");
+
+console.log("8x1 = ", 8 * 1);
+console.log("8x2 = ", 8 * 2);
+console.log("8x3 = ", 8 * 3);
+console.log("8x4 = ", 8 * 4);
+console.log("8x5 = ", 8 * 5);
+console.log("8x6 = ", 8 * 6);
+console.log("8x7 = ", 8 * 7);
+console.log("8x8 = ", 8 * 8);
+console.log("8x9 = ", 8 * 9);
+console.log("8x10 = ", 8 * 10);
+
+
+
+//SEGUNDA TABUADA//
+
+
+
+
+let T1 = 1;
+let T2 = 2;
+let T3 = 3;
+let T4 = 4;
+let T5 = 5;
+let T6 = 6;
+let T7 = 7;
+let T8 = 8;
+let T9 = 9;
+let T10 = 10;
+
+console.log("=========================")
+
+console.log(T1 + "x1=", T1* 1);
+console.log(T1 + "x2=", T1* 2);
+console.log(T1 + "x3=", T1* 3);
+console.log(T1 + "x4=", T1* 4);
+console.log(T1 + "x5=", T1* 5);
+console.log(T1 + "x6=", T1* 6);
+console.log(T1 + "x7=", T1* 7);
+console.log(T1 + "x8=", T1* 8);
+console.log(T1 + "x9=", T1* 9);
+console.log(T1 + "x10=", T1 * 10);
+
+console.log("=========================")
+
+console.log(T2 + "x1=", T2 * 1);
+console.log(T2 + "x2=", T2 * 2);
+console.log(T2 + "x3=", T2 * 3);
+console.log(T2 + "x4=", T2 * 4);
+console.log(T2 + "x5=", T2 * 5);
+console.log(T2 + "x6=", T2 * 6);
+console.log(T2 + "x7=", T2 * 7);
+console.log(T2 + "x8=", T2 * 8);
+console.log(T2 + "x9=", T2 * 9);
+console.log(T2 + "x10=", T2 * 10);
+
+console.log("=========================")
+
+console.log(T3 + "x1=", T3* 1);
+console.log(T3 + "x2=", T3* 2);
+console.log(T3 + "x3=", T3* 3);
+console.log(T3 + "x4=", T3* 4);
+console.log(T3 + "x5=", T3* 5);
+console.log(T3 + "x6=", T3* 6);
+console.log(T3 + "x7=", T3* 7);
+console.log(T3 + "x8=", T3* 8);
+console.log(T3 + "x9=", T3* 9);
+console.log(T3 + "x10=", T3 * 10);
+
+console.log("=========================")
+
+console.log(T4 + "x1=", T4 * 1);
+console.log(T4 + "x2=", T4 * 2);
+console.log(T4 + "x3=", T4 * 3);
+console.log(T4 + "x4=", T4 * 4);
+console.log(T4 + "x5=", T4 * 5);
+console.log(T4 + "x6=", T4 * 6);
+console.log(T4 + "x7=", T4 * 7);
+console.log(T4 + "x8=", T4 * 8);
+console.log(T4 + "x9=", T4 * 9);
+console.log(T4 + "x10=", T4 * 10);
+
+console.log("=========================")
+
+console.log(T5 + "x1=", T5 * 1);
+console.log(T5 + "x2=", T5 * 2);
+console.log(T5 + "x3=", T5 * 3);
+console.log(T5 + "x4=", T5 * 4);
+console.log(T5 + "x5=", T5 * 5);
+console.log(T5 + "x6=", T5 * 6);
+console.log(T5 + "x7=", T5 * 7);
+console.log(T5 + "x8=", T5 * 8);
+console.log(T5 + "x9=", T5 * 9);
+console.log(T5 + "x10=", T5 * 10);
+
+console.log("=========================")
+
+console.log(T6 + "x1=", T6 * 1);
+console.log(T6 + "x2=", T6 * 2);
+console.log(T6 + "x3=", T6 * 3);
+console.log(T6 + "x4=", T6 * 4);
+console.log(T6 + "x5=", T6 * 5);
+console.log(T6 + "x6=", T6 * 6);
+console.log(T6 + "x7=", T6 * 7);
+console.log(T6 + "x8=", T6 * 8);
+console.log(T6 + "x9=", T6 * 9);
+console.log(T6 + "x10=", T6 * 10);
+
+console.log("=========================")
+
+console.log(T7 + "x1=", T7 * 1);
+console.log(T7 + "x2=", T7 * 2);
+console.log(T7 + "x3=", T7 * 3);
+console.log(T7 + "x4=", T7 * 4);
+console.log(T7 + "x5=", T7 * 5);
+console.log(T7 + "x6=", T7 * 6);
+console.log(T7 + "x7=", T7 * 7);
+console.log(T7 + "x8=", T7 * 8);
+console.log(T7 + "x9=", T7 * 9);
+console.log(T7 + "x10=", T7 * 10);
+
+console.log("=========================")
+
+console.log(T8 + "x1=", T8 * 1)
+console.log(T8 + "x2=", T8 * 2);
+console.log(T8 + "x3=", T8 * 3);
+console.log(T8 + "x4=", T8 * 4);
+console.log(T8 + "x5=", T8 * 5);
+console.log(T8 + "x6=", T8 * 6);
+console.log(T8 + "x7=", T8 * 7);
+console.log(T8 + "x8=", T8 * 8);
+console.log(T8 + "x9=", T8 * 9);
+console.log(T8 + "x10=", T8 * 10);
+
+console.log("=========================")
+
+console.log(T9 + "x1=", T9 * 1);
+console.log(T9 + "x2=", T9 * 2);
+console.log(T9 + "x3=", T9 * 3);
+console.log(T9 + "x4=", T9 * 4);
+console.log(T9 + "x5=", T9 * 5);
+console.log(T9 + "x6=", T9 * 6);
+console.log(T9 + "x7=", T9 * 7);
+console.log(T9 + "x8=", T9 * 8);
+console.log(T9 + "x9=", T9 * 9);
+console.log(T9 + "x10=", T9 * 10);
+
+console.log("=========================")
+
+console.log(T10 + "x1=", T10 * 1);
+console.log(T10 + "x2=", T10 * 2);
+console.log(T10 + "x3=", T10 * 3);
+console.log(T10 + "x4=", T10 * 4);
+console.log(T10 + "x5=", T10 * 5);
+console.log(T10 + "x6=", T10 * 6);
+console.log(T10 + "x7=", T10 * 7);
+console.log(T10 + "x8=", T10 * 8);
+console.log(T10 + "x9=", T10 * 9);
+console.log(T10 + "x10=", T10 * 10);
+
+console.log("=========================")
+
+//TERCEIRA TABUADA//
+
+let T = 64;
+console.log(T = T + 1);
