1d2be92ed23d50b62dc56ae08ce7df2d4dc3a06b
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-05-21 10:12:03 -0300

    Create nome.js

---
+++ nome.js
@@ -0,0 +1,13 @@
+const nome = "Murilo"
+const sobrenome = "Reis"
+
+let nomecompleto = ""
+nomecompleto = nome + " " + sobrenome
+console.log("Meu nome é:" , nomecompleto)
+
+
+
+x = Number("8")
+h = String (8)
+o = "8" + "5"
+console.log("Valor é:", o)
