fe01e668da493daea6f29bec6dcb4e0e340ecc3e
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-06-11 08:55:21 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+//lista1 = ["Leandro", "Maria", "Mônica"]
+//lista2 = ["Fassina", "Silva", "Fabio"]
+//listacompleta = lista1.concat(lista2)
+
+//console.log(listacompleta)
+
+//dado = [["Marcos", "Rafaela", "Maria"],[ 8, 8.5, 9]];
+//console.log("nome: " + dado [0][1]+ " média: " + dado [1][1])
+
+dado2 = [["Marcos", "Rafaela", "Maria"],[[3, 9, 7],[5, 8, 8, 9],[8, 9 ,7]]];
+console.log("nome: " + dado2 [0][1]+ " média: " + dado2 [1][1][0]);
+
+
